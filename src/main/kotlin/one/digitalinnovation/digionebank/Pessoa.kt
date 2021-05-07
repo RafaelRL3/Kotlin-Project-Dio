d54e8a6328dcb54cf226810a19e2edb7d242467a
RafaelRL3 <rafellabio2@gmail.com>
2021-05-06 23:40:45 -0300

    Update to apply the private modifier to set function of cpf propety

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,19 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Rafael"
-    var cpf: String = "123.123.123-11"
+
+
+    var cpf: String = "123.123.123-11" // eh inicializado mas n pode ser mexido
+    private  set //Estou tornando privado a propiedade de alterar as var
+
 }
 
 fun main(){
-    val Rafael = Pessoa()
+    val rafael = Pessoa()
+    rafael.nome = "rafael" //Estou setando aq
 
-    println(Rafael.cpf)
-    println(Rafael.nome)
+    println(rafael)
+    println(rafael.nome) //Estou recebendo, vulgo get. cpf
+    println(rafael.cpf)
+
 }
