57bbbac0e1a9a7cf0b349d20098d1337cb42c5e5
RafaelRL3 <rafellabio2@gmail.com>
2021-05-07 00:12:55 -0300

    update, using members of class and constructor

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -6,15 +6,15 @@ class Pessoa {
 
     var cpf: String = "123.123.123-11" // eh inicializado mas n pode ser mexido
     private  set //Estou tornando privado a propiedade de alterar as var
+    constructor()
+
+    fun PessoaIn() = "$nome e $cpf"
 
 }
 
 fun main(){
     val rafael = Pessoa()
     rafael.nome = "rafael" //Estou setando aq
-
-    println(rafael)
-    println(rafael.nome) //Estou recebendo, vulgo get. cpf
-    println(rafael.cpf)
+    println(rafael.PessoaIn())
 
 }
