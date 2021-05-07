01574380e32f436437cfe59a827eee5943842498
RafaelRL3 <rafellabio2@gmail.com>
2021-05-07 16:30:42 -0300

    Created Data Bank class and Bank test

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/BancoTest.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main(){
+    val digiOneBank = Banco(numero = 12, nome = "DigiOne")
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    val banco2 = digiOneBank.copy(nome = "banco2")
+    println(banco2.info())
+}
